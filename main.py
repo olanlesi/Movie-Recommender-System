aa4dd9ec03ae9af3ac1d3802f820e3512c9fe0ff
Olanlesi Banjo <75541792+olanlesi@users.noreply.github.com>
2022-10-23 12:01:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import json
 import bs4 as bs
+from nltk.corpus import stopwords
 import urllib.request
 import pickle
 import requests
